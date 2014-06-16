aecc3af232658e1d4e1a106b3500f7c00db3884e
Zach Sullivan <zach.r.sullivan@gmail.com>
2014-06-16 10:04:17 -0400

    Project will now close when 2 options have been chosen

--- immersive_Unity/Assets/Scripts/CharacterResponses.cs
+++ immersive_Unity/Assets/Scripts/CharacterResponses.cs
@@ -12,105 +12,112 @@ public class CharacterResponses : MonoBehaviour {
 
 	private Animator anim;
 		
+	int choiceCounter;
+
 	void Start() {
 		anim = GetComponent<Animator>();
-		}
+	}
+
+	void Update(){
+
+		print ("ChoiceCounter = " + choiceCounter);
+	}
 
 	public void checkResponse(int caseNum){
 		switch(caseNum){
 		
 		case 1:
 			print ("case1 ACTIVATE!");
-			//splineMover.startMove();
-			//waypointMove.currentWaypoint = waypoints[0];
+
 			iTweenEvent.GetEvent(AI,"TVPathEvent").Play();
 			aiLook.enabled = false;
 			anim.SetBool("isWalking", true);
+
+			choiceCounter++;
 			LeaveDialog();
 			break;
 		
 		case 2:
 			print ("case2 ACTIVATE!");
+
 			iTweenEvent.GetEvent(AI,"KitchenPathEvent").Play();
 			aiLook.enabled = false;
 			anim.SetBool("isWalking", true);
+
+			choiceCounter++;
 			LeaveDialog();
 		break;	
 		
 		case 3:
 			print ("case3 ACTIVATE!");
+
+			choiceCounter++;
 			LeaveDialog();
 			break;
 		
 		case 4:
 			print ("case4 ACTIVATE!");
+
+			choiceCounter++;
 			LeaveDialog();
 			break;
 
 		case 5:
 			print ("case5 ACTIVATE!");
+
+			choiceCounter++;
 			LeaveDialog();
 			break;
 			
 		case 6:
 			print ("case6 ACTIVATE!");
+
 			iTweenEvent.GetEvent(AI,"TVPathEvent").Play();
 			aiLook.enabled = false;
 			anim.SetBool("isWalking", true);
+
+			choiceCounter++;
 			LeaveDialog();
 			break;	
 			
 		case 7:
 			print ("case7 ACTIVATE!");
+
 			iTweenEvent.GetEvent(AI,"TVPathEvent").Play();
 			aiLook.enabled = false;
 			anim.SetBool("isWalking", true);
+
+			choiceCounter++;
 			LeaveDialog();
 			break;
 			
 		case 8:
 			print ("case8 ACTIVATE!");
+
 			iTweenEvent.GetEvent(AI,"TVPathEvent").Play();
 			aiLook.enabled = false;
 			anim.SetBool("isWalking", true);
+
+			choiceCounter++;
 			LeaveDialog();
 			break;
 
 		case 9:
 			print ("case9 ACTIVATE!");
-			LeaveDialog();
 
+			choiceCounter++;
+			LeaveDialog();
 			break;
 
 		default:
 			break;
 		}
 	}
-
-	/*
-	void WriteToFile(){
-
-		// Create an instance of StreamWriter to write text to a file.
-		StreamWriter sw = new StreamWriter("TestFile.txt");
-		// Add some text to the file.
-		sw.Write("This is the ");
-		sw.WriteLine("header for the file.");
-		sw.WriteLine("-------------------");
-		// Arbitrary objects can also be written to the file.
-		sw.Write("The date is: ");
-		sw.WriteLine(DateTime.Now);
-		sw.WriteLine("-------------------");
-		sw.WriteLine(dialogue.responseNum);
-		sw.WriteLine("-------------------");
-		sw.Close();
-	}
-	*/
-
+	
 	void AddToFile(){
-		//StreamWriter sw = new StreamWriter("TestFile.txt");
+
 		StreamWriter sw = File.AppendText ("TestFile.xml");
-		//sw.Write ("This line was appended.");
-		//sw.WriteLine ("This one too.");
+
 		sw.WriteLine ("<?xml version='1.0' encoding='UTF-8'?>");
 		sw.Write ("<Scene>");
 
@@ -130,13 +137,19 @@ public class CharacterResponses : MonoBehaviour {
 		
 		GameObject.FindWithTag("PlayerArms").GetComponent<Animation>().enabled = true;
 
-		//WriteToFile();
 		AddToFile();
 
 		print("Exited Dialog!");
 	}
 
 	void eventEnd(){
+
+		if (choiceCounter == 2){
+			iTween.CameraFadeAdd ();
+			iTween.CameraFadeFrom (1,1);
+			Application.Quit();
+		}
+
 		anim.SetBool("isWalking", false);
 		aiLook.enabled = true;
 	}
---
+++ immersive_Unity/Assets/Scripts/Menu_Script.js
@@ -0,0 +1,22 @@
+﻿public var loadingMessage : GameObject;
+
+function Start () {
+	loadingMessage.renderer.enabled = false;
+}
+
+function OnGUI () {
+          // Make a background box
+    GUI.Box (Rect (Screen.width/2-100,Screen.height/2+20,200,80), "");
+
+    // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+    if (GUI.Button (Rect (Screen.width/2-90,Screen.height/2+40,180,20), "Menu")) {
+        loadingMessage.renderer.enabled = true;
+        Application.LoadLevel ("Menu");
+    }
+
+    // Make the second button.
+    if (GUI.Button (Rect (Screen.width/2-90,Screen.height/2+70,180,20), "Kitchen Scene")) {
+    	loadingMessage.renderer.enabled = true;
+        Application.LoadLevel ("kitchen_scene");
+    }
+}
