85354cad64daddc0269847b3b917f16f833b2602
Zach Sullivan <zach.r.sullivan@gmail.com>
2014-06-26 13:42:34 -0400

    Added radial menu

--- immersive_Unity/Assets/Scripts/CharacterInteract.cs
+++ immersive_Unity/Assets/Scripts/CharacterInteract.cs
@@ -51,8 +51,7 @@ public class CharacterInteract : MonoBehaviour {
 	}
 
 	void Update(){
-		//isSitting = anim.GetBool ("isSitting");
-
+		isSitting = anim.GetBool ("isSitting");
 		/*Vector3 point = targetFollow.position;
 		Vector3 v3Dir = targetFollow.position - transform.position;
 
@@ -83,9 +82,8 @@ public class CharacterInteract : MonoBehaviour {
 			rotationReset = true;
 
 			//Now that the object has been used, it must always face towards the player.
-			//if(targetFollow != null && isSitting != true){
-			if(targetFollow != null){
-
+			if(targetFollow != null && isSitting != true){
+              		
 				 	var lookPos = targetFollow.position - transform.position;
 					lookPos.y = 0;
 					
--- immersive_Unity/Assets/Scripts/DialogGUI.cs
+++ immersive_Unity/Assets/Scripts/DialogGUI.cs
@@ -16,7 +16,42 @@ public class options
 }
 
 public class DialogGUI : MonoBehaviour {
+
+	public GameObject controllerArrow;
+	public GameObject Option1;
+	public GameObject Option2;
+	public GameObject Option3;
+	public GameObject Option4;
+	public GameObject Option5;
+	public GameObject Option6;
+	public GameObject Option7;
+	public GameObject Option8;
+	public GameObject Option9;
+
+	public Material off_1;
+	public Material off_2;
+	public Material off_3;
+	public Material off_4;
+	public Material off_5;
+	public Material off_6;
+	public Material off_7;
+	public Material off_8;
+	public Material off_9;
+
+	public float y;
+	public float x;
 	
+	public Vector2 centerVector;
+	public Vector2 stickVector;
+	public Vector3 v3;
+
+	public string text;
+	public float angle;
+	
+	public float test;
+	public float deltaX;
+	public float deltaY;
+
 	private CharacterInteract state;
 	private Vector3 scale;
 
@@ -34,7 +69,118 @@ public class DialogGUI : MonoBehaviour {
 		state = gameObject.GetComponent<CharacterInteract>();
 		responseNum = 0;
 	}
-	
+
+	void Update () {
+		x = Input.GetAxis("JoyHorizontal");
+		y = Input.GetAxis("JoyVertical");
+		
+		stickVector.x = x;
+		stickVector.y = y;
+
+		centerVector.x = 0.0f;
+		centerVector.y = 0.0f;
+		
+		angle = Vector2.Angle(stickVector, centerVector);
+		
+		deltaX = x - 0;
+		deltaY = y - 0;
+		
+		test = Mathf.Atan2(deltaY, deltaX) * 180 / 3.14f;
+
+		v3.x = -test;
+		v3.y = 90;
+		v3.z = -90;
+
+		controllerArrow.transform.localEulerAngles = v3;
+
+		//Debug.Log (test);
+
+		Option1.renderer.material = off_1;
+		Option2.renderer.material = off_2;
+		Option3.renderer.material = off_3;
+		Option4.renderer.material = off_4;
+		Option5.renderer.material = off_5;
+		Option6.renderer.material = off_6;
+		Option7.renderer.material = off_7;
+		Option8.renderer.material = off_8;
+		Option9.renderer.material = off_9;
+
+		if (test < 72 && test > 36) {
+			//Option1.renderer.enabled = false;
+			Option1.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test");
+				responseNum = 1;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < 36 && test > 0) {
+			Option2.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test2");
+				responseNum = 2;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < 0 && test > -36) {
+			Option3.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test3");
+				responseNum = 3;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < -36 && test > -72) {
+			Option4.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test4");
+				responseNum = 4;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < -72 && test > -108) {
+			Option5.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test5");
+				responseNum = 5;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < -108 && test > -138) {
+			Option6.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test6");
+				responseNum = 6;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < -144 && test > -179) {
+			Option7.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test7");
+				responseNum = 7;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < 179 && test > 144) {
+			Option8.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test8");
+				responseNum = 8;
+				response.checkResponse(responseNum);
+			}
+		}
+		if (test < 144 && test > 108) {
+			Option9.renderer.material.color = Color.red;
+			if (Input.GetKeyDown(KeyCode.JoystickButton0)){
+				changeDescription("test9");
+				responseNum = 9;
+				response.checkResponse(responseNum);
+			}
+		}
+	}
+
+	/*
 	void OnGUI(){
 
 		scale.x = Screen.width / originalWidth;
@@ -74,7 +220,7 @@ public class DialogGUI : MonoBehaviour {
 		if (GUI.Button (new Rect (50, 40, buttonWidth, buttonHeight), dialogMenu.option1, guiSpacing)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test");
+			changeDescription("test");
 			responseNum = 1;
 			response.checkResponse(responseNum);
 		}
@@ -82,14 +228,14 @@ public class DialogGUI : MonoBehaviour {
 		if (GUI.Button (new Rect (370, 40, buttonWidth, buttonHeight), dialogMenu.option2, guiSpacing)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test2");
+			changeDescription("test2");
 			responseNum = 2;
 			response.checkResponse(responseNum);
 		}
 		if (GUI.Button (new Rect (10, 100, buttonWidth, buttonHeight), dialogMenu.option3)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test3");
+			changeDescription("test3");
 			responseNum = 3;
 			response.checkResponse(responseNum);
 		}
@@ -97,7 +243,7 @@ public class DialogGUI : MonoBehaviour {
 		if (GUI.Button (new Rect (10, 160, buttonWidth, buttonHeight), dialogMenu.option4)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test4");	
+			changeDescription("test4");	
 			responseNum = 4;
 			response.checkResponse(responseNum);
 		}
@@ -105,7 +251,7 @@ public class DialogGUI : MonoBehaviour {
 		if (GUI.Button (new Rect (420, 100, buttonWidth, buttonHeight), dialogMenu.option5)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test5");
+			changeDescription("test5");
 			responseNum = 5;
 			response.checkResponse(responseNum);
 		}
@@ -113,7 +259,7 @@ public class DialogGUI : MonoBehaviour {
 		if (GUI.Button (new Rect (420, 160, buttonWidth, buttonHeight), dialogMenu.option6)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test6");
+			changeDescription("test6");
 			responseNum = 6;
 			response.checkResponse(responseNum);
 		}
@@ -121,21 +267,21 @@ public class DialogGUI : MonoBehaviour {
 		if (GUI.Button (new Rect (50, 220, buttonWidth, buttonHeight), dialogMenu.option7)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test7");
+			changeDescription("test7");
 			responseNum = 7;
 			response.checkResponse(responseNum);
 		}
 		if (GUI.Button (new Rect (210, 220, buttonWidth, buttonHeight), dialogMenu.option8)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test8");
+			changeDescription("test8");
 			responseNum = 8;
 			response.checkResponse(responseNum);
 		}
 		if (GUI.Button (new Rect (370, 220, buttonWidth, buttonHeight), dialogMenu.option9)) {
 			
 			print("Button Clicked!");
-			//changeDescription("test9");
+			changeDescription("test9");
 			responseNum = 9;
 			response.checkResponse(responseNum);
 		}
@@ -145,8 +291,8 @@ public class DialogGUI : MonoBehaviour {
 		//if (Input.GetKeyDown(KeyCode.Escape) || (Input.GetButton("Exit"))) {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			state.itemUseable = true;
-			//GameObject.FindWithTag("Description").GetComponent<GUIText>().text = "";
-			//GameObject.FindWithTag("Description").GetComponent<GUIText>().enabled = false;
+			GameObject.FindWithTag("Description").GetComponent<GUIText>().text = "";
+			GameObject.FindWithTag("Description").GetComponent<GUIText>().enabled = false;
 
 			GameObject.FindWithTag("PlayerArms").GetComponent<Animation>().enabled = true;
 
@@ -156,9 +302,10 @@ public class DialogGUI : MonoBehaviour {
 		GUI.matrix = guiMat;
 
 	}
+	*/
 
 	void changeDescription (string description){
 
-		GameObject.FindWithTag("Description").GetComponent<GUIText>().text = description;
+		//GameObject.FindWithTag("Description").GetComponent<GUIText>().text = description;
 	}
 }
