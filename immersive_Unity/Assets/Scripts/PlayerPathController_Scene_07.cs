f5cb1932661f1d83f66ca2b26a429e093c4ec254
Zach Sullivan <zach.r.sullivan@gmail.com>
2014-08-11 11:14:50 -0400

    Added Dialog sounds

---
+++ immersive_Unity/Assets/Scripts/PlayerPathController_Scene_07.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class audio_Scene07_start {
+	public AudioClip clip1;
+	public AudioClip clip2;
+	public AudioClip clip3;
+	public AudioClip clip4;
+	public AudioClip clip5;
+}
+
+
+public class PlayerPathController_Scene_07 : MonoBehaviour {
+
+
+	private CharacterInteract_Scene_07 AiInteract;
+	public GameObject Player;
+	public audio_Scene07_start  audioStart;
+
+
+	// Use this for initialization
+	void Start () {
+		AiInteract = GameObject.Find ("Antagonist").GetComponent<CharacterInteract_Scene_07> ();
+
+		StartCoroutine ("StartIntro");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnComplete(){
+
+		AiInteract.itemUseable = false;
+	}
+	
+	IEnumerator  StartIntro(){
+		/*
+		audio.clip = audioStart.clip1;
+		audio.Play ();
+		yield return new WaitForSeconds(audio.clip.length);
+
+		audio.clip = audioStart.clip2;
+		audio.Play ();
+
+		yield return new WaitForSeconds(audio.clip.length);
+		*/
+		audio.clip = audioStart.clip3;
+		audio.Play ();
+
+		yield return new WaitForSeconds(audio.clip.length);
+		
+		/*audio.clip = audioStart.clip4;
+		audio.Play ();
+
+		yield return new WaitForSeconds(audio.clip.length);
+		
+		audio.clip = audioStart.clip5;
+		audio.Play ();
+
+		yield return new WaitForSeconds(audio.clip.length);*/
+
+		OnComplete ();
+	}
+}
