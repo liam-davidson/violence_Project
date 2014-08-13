40e7e88b0f29a91842a6f5afcf5f9a9ff66e8650
Zach Sullivan <zach.r.sullivan@gmail.com>
2014-08-13 11:33:50 -0400

    Added Scene8 with audio dialog

---
+++ immersive_Unity/Assets/Scripts/PlayerPathController_Scene_08.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class audio_Scene08_start {
+	public AudioClip clip1;
+	public AudioClip clip2;
+}
+
+
+public class PlayerPathController_Scene_08 : MonoBehaviour {
+
+
+	private CharacterInteract_Scene_08 AiInteract;
+	public GameObject Player;
+	public audio_Scene08_start  audioStart;
+
+
+	// Use this for initialization
+	void Start () {
+		AiInteract = GameObject.Find ("Antagonist").GetComponent<CharacterInteract_Scene_08> ();
+
+		StartCoroutine ("StartIntro");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnComplete(){
+
+		AiInteract.itemUseable = false;
+	}
+	
+	IEnumerator  StartIntro(){
+	
+
+		audio.clip = audioStart.clip1;
+		audio.Play ();
+
+		yield return new WaitForSeconds(audio.clip.length);
+	
+		audio.clip = audioStart.clip2;
+		audio.Play ();
+
+		yield return new WaitForSeconds(audio.clip.length);
+
+		OnComplete ();
+	}
+}
